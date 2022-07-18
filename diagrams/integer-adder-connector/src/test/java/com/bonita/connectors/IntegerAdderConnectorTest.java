9a323a380648c2dc004a32c6c0efd9d98854c78f
goenkale <goenkale@gmail.com>
2022-07-18 17:47:35 +0200

    Initial commit of 'Custom Connector Maven Archetype'

---
+++ diagrams/integer-adder-connector/src/test/java/com/bonita/connectors/IntegerAdderConnectorTest.java
@@ -0,0 +1,79 @@
+package com.bonita.connectors;
+
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class IntegerAdderConnectorTest {
+
+    IntegerAdderConnector connector;
+
+    @BeforeEach
+    void setUp() {
+        connector = new IntegerAdderConnector();
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_is_missing() {
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_1_is_empty() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("firstInteger", "");
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+    @Test
+    void should_throw_exception_if_mandatory_input_2_is_empty() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("secondInteger", "");
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_1_is_not_an_integer() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("firstInteger", 50);
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+    
+    @Test
+    void should_throw_exception_if_mandatory_input_2_is_not_an_integer() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("secondInteger", 10);
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_create_output_for_valid_input() throws ConnectorException {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("firstInteger", "50");
+        parameters.put("secondInteger", "10");
+        connector.setInputParameters(parameters);
+        Map<String, Object> outputs = connector.execute();
+        assertThat(outputs).containsEntry("result", "60");
+    }
+
+}
